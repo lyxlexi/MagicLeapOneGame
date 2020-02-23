9a95c6b45fc0b1eb9c11d8ac521daf83affa89ee
January Shen <chi12@uw.edu>
2020-02-22 18:37:30 -0800

    control scripts

---
+++ Assets/PlaceObject.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+public class PlaceObject : MonoBehaviour
+{
+    public GameObject ObjectToPlace;
+    private MLInputController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MLInput.Start();
+        MLInput.OnControllerButtonDown += OnButtonDown;
+        controller = MLInput.GetController(MLInput.Hand.Left);
+    }
+
+    void OnButtonDown(byte controller_id, MLInputControllerButton button)
+    {
+        if(button == MLInputControllerButton.Bumper)
+        {
+            RaycastHit hit;
+            if(Physics.Raycast(controller.Position, transform.forward, out hit))
+            {
+                GameObject PlaceObject = Instantiate(ObjectToPlace, hit.point, Quaternion.Euler(hit.normal));
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        MLInput.Stop();
+        MLInput.OnControllerButtonDown -= OnButtonDown;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
