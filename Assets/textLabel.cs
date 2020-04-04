1e1a3afdb259ee6c519cd4577ac2afd1d6d078b2
lyxlexi <lyx.lexi@gmail.com>
2020-04-03 20:44:03 -0700

    mainScene Textw/Jupyter

---
+++ Assets/textLabel.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExampleClass : MonoBehaviour
+{
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), "Hello World!");
+    }
+}
