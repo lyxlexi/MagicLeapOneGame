99b35984cfe5f6eddb881e4c1532e763ec469e4a
January Shen <chi12@uw.edu>
2020-04-27 15:09:47 -0700

    added final animation

---
+++ Assets/Jan_changeScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jan_changeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if(Application.loadedLevel == 0)
+            {
+                Application.LoadLevel(1);
+            }
+            else
+            {
+                Application.LoadLevel(0);
+            }
+        }
+    }
+}
---
+++ Assets/Prefab/AnimEvents.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimEvents : MonoBehaviour
+{
+    void showMsg(string msg, int count)
+    {
+        Debug.Log(msg);
+    }
+}
